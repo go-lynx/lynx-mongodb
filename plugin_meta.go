30a71359a8aea20239eb845fbc6e1efed42ae061
tan-zhuo <tanzhuo@aliyun.com>
2026-05-27 22:12:28 +0900

    chore: bump pluginVersion to v1.6.1

--- plugin_meta.go
+++ plugin_meta.go
@@ -9,7 +9,7 @@ const (
 	// Plugin unique name
 	pluginName = "mongodb.client"
 	// Plugin version number
-	pluginVersion = "v1.6.0-beta"
+	pluginVersion = "v1.6.1"
 	// Plugin description
 	pluginDescription = "mongodb plugin for lynx framework"
 	// Configuration prefix, used to read plugin-related configuration from config
